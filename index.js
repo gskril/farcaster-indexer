af13c1a025da040b7ebf6860313522f4ae254549
Greg <gskril@gmail.com>
2022-06-14 20:00:23 -0400

    Index profiles and casts in DB

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 	serverApi: ServerApiVersion.v1,
 })
 
+const provider = new providers.AlchemyProvider(
+	'rinkeby',
+	process.env.ALCHEMY_SECRET
+)
+
+const registryContract = new Contract(
+	'0xe3Be01D99bAa8dB9905b33a3cA391238234B79D1',
+	require('./registry-abi.js'),
+	provider
+)
+
 function indexCasts() {
 	client.connect(async (err) => {
 		if (err) {
@@ -30,32 +41,89 @@ function indexCasts() {
 		// Avoid indexing duplicate casts
 		await newCollection.createIndex({ merkleRoot: 1 }, { unique: true })
 
-		const provider = new providers.AlchemyProvider(
-			'rinkeby',
-			process.env.ALCHEMY_SECRET
-		)
-
-		const registryContract = new Contract(
-			'0xe3Be01D99bAa8dB9905b33a3cA391238234B79D1',
-			require('./registry-abi.js'),
-			provider
-		)
-
-		let usersIndexed = 0
-		const numberOfUsers = await registryContract
-			.usernamesLength()
+		let profilesIndexed = 0
+		const profiles = await db
+			.collection('profiles')
+			.find({})
+			.toArray()
 			.catch(() => {
-				console.error('Error getting number of users from contract')
+				console.error('Error getting number of profiles from MongoDB')
 				return 0
 			})
 
-		if (numberOfUsers === 0) {
-			return
-		} else {
-			console.log(`Indexing ${numberOfUsers} users...`)
+		if (profiles.length === 0) return
+		console.log(`Indexing casts from ${profiles.length} profiles...`)
+
+		for (let i = 0; i < profiles.length; i++) {
+			try {
+				const activityUrl = profiles[i].body.addressActivityUrl
+				const activity = await got(activityUrl).json()
+
+				if (activity.length > 0) {
+					await newCollection
+						.insertMany(activity)
+						.then(() => profilesIndexed++)
+						.catch((err) => {
+							console.log(
+								`Error saving ${username}'s casts.`,
+								err.message
+							)
+						})
+				}
+			} catch (err) {
+				// console.log(`Unable to get ${username}'s activity`)
+			}
 		}
 
-		for (let i = 0; i < numberOfUsers; i++) {
+		// Replace existing collection with new casts
+		try {
+			await oldConnection.drop()
+		} catch (err) {
+			console.log('Error dropping collection.', err.codeName)
+		}
+		await newCollection.rename('casts')
+
+		client.close()
+		const endTime = Date.now()
+		const secondsTaken = (endTime - startTime) / 1000
+		console.log(
+			`Indexed casts from ${profilesIndexed} profiles in ${secondsTaken} seconds`
+		)
+	})
+}
+
+function indexProfiles() {
+	client.connect(async (err) => {
+		if (err) {
+			console.error('Error connecting to MongoDB.', err)
+			return
+		}
+
+		const startTime = Date.now()
+		const db = client.db('farcaster')
+		const oldConnection = db.collection('profiles')
+		const newCollection = db.collection('profiles_temp')
+
+		// If the temp table already exists, drop it
+		try {
+			await newCollection.drop()
+		} catch {}
+
+		// Avoid indexing duplicate profiles
+		await newCollection.createIndex({ merkleRoot: 1 }, { unique: true })
+
+		let profilesIndexed = 0
+		const numberOfProfiles = await registryContract
+			.usernamesLength()
+			.catch(() => {
+				console.error('Error getting number of profiles from contract')
+				return 0
+			})
+
+		if (numberOfProfiles === 0) return
+		console.log(`Indexing ${numberOfProfiles} profiles...`)
+
+		for (let i = 0; i < numberOfProfiles; i++) {
 			const byte32Name = await registryContract
 				.usernameAtIndex(i)
 				.catch(() => {
@@ -77,51 +145,53 @@ function indexCasts() {
 					return null
 				})
 
-			if (!directoryUrl) continue
+			if (!directoryUrl || directoryUrl.includes('localhost')) continue
 
 			try {
-				const activityUrl = await got(directoryUrl)
+				const directory = await got(directoryUrl)
 					.json()
-					.then((res) => res.body)
-					.then((res) => res.addressActivityUrl)
+					.then((res) => {
+						res.index = i
+						return res
+					})
 
-				const activity = await got(activityUrl).json()
-
-				if (activity.length > 0) {
-					await newCollection
-						.insertMany(activity)
-						.then(() => usersIndexed++)
-						.catch((err) => {
-							console.log(
-								`Error saving ${username}'s casts.`,
-								err.message
-							)
-						})
-				}
+				await newCollection
+					.insertOne(directory)
+					.then(() => profilesIndexed++)
+					.catch((err) => {
+						console.log(
+							`Error saving ${username}'s directory.`,
+							err.message
+						)
+					})
 			} catch (err) {
-				// Ignore issues with testing accounts
-				if (!directoryUrl.includes('localhost')) {
-					// console.log(`Unable to get ${username}'s activity at directory ${directoryUrl}`)
-				}
+				// console.log(`Unable to get ${username}'s directory`)
 			}
 		}
 
-		// Replace existing collection with new casts
+		// Replace existing collection with new one
 		try {
 			await oldConnection.drop()
 		} catch (err) {
 			console.log('Error dropping collection.', err.codeName)
 		}
-		await newCollection.rename('casts')
+		await newCollection.rename('profiles')
 
 		client.close()
 		const endTime = Date.now()
 		const secondsTaken = (endTime - startTime) / 1000
-		console.log(`Indexed ${usersIndexed} users in ${secondsTaken} seconds`)
+		console.log(
+			`Indexed ${profilesIndexed} directories in ${secondsTaken} seconds`
+		)
 	})
 }
 
 // Run job every hour
-cron.schedule('0 */1 * * *', async () => {
+cron.schedule('0 */1 * * *', () => {
+	indexProfiles()
+})
+
+// Run job 10 mins
+cron.schedule('10 */1 * * *', () => {
 	indexCasts()
 })
