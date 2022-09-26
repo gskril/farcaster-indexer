2e7bf7683dc09f5074d3e87c5768a70a153d4919
Greg <gskril@gmail.com>
2022-09-26 02:29:04 +0100

    Add cast types

--- src/types/index.ts
+++ src/types/index.ts
@@ -35,3 +35,71 @@ export interface FlattenedProfile {
   registered_at?: Date
   updated_at?: Date
 }
+
+export interface Cast {
+  body: {
+    type: 'text-short'
+    publishedAt: number
+    sequence: number
+    address: string
+    username: string
+    data: {
+      text: string
+      replyParentMerkleRoot: string
+    }
+    prevMerkleRoot: string
+  }
+  signature: string
+  merkleRoot: string
+  threadMerkleRoot: string
+  meta: {
+    displayName: string
+    avatar: string
+    isVerifiedAvatar: boolean
+    mentions: {
+      address: string
+      username: string
+    }[]
+    numReplyChildren: number
+    replyParentUsername: {
+      address: string
+      username: string
+    }
+    reactions: {
+      count: number
+    }
+    recasters: Profile[]
+    recasts: {
+      count: number
+    }
+    watches: {
+      count: number
+    }
+  }
+}
+
+export interface FlattenedCast {
+  type: 'text-short'
+  published_at: number
+  sequence: number
+  address: string
+  username: string
+  text: string
+  reply_parent_merkle_root: string
+  prev_merkle_root: string
+  signature: string
+  merkle_root: string
+  thread_merkle_root: string
+  display_name: string
+  avatar_url: string
+  avatar_verified: boolean
+  mentions: JSON
+  num_reply_children: number
+  reply_parent_username: string
+  reply_parent_address: string
+  reactions: number
+  recasts: number
+  watches: number
+  recasters: JSON
+  deleted: boolean
+}
