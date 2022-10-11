1bd43d5e815c0833aa12cb87e712b1c3644f3a36
Tiago Alves <alvesjtiago@gmail.com>
2022-10-11 17:03:18 +0100

    Refactor cast to flattened cast

--- src/functions/index-casts.ts
+++ src/functions/index-casts.ts
@@ -1,8 +1,9 @@
-import { breakIntoChunks, cleanUserActivity } from '../utils.js'
+import { breakIntoChunks } from '../utils.js'
 import { castsTable, profilesTable } from '../index.js'
 import got from 'got'
 import supabase from '../supabase.js'
 import { spawn, Thread, Worker } from 'threads'
+import flattenCast from '../utils/flattenCast.js'
 
 import { Cast, FlattenedCast, FlattenedProfile } from '../types/index'
 
@@ -11,6 +12,8 @@ import { Cast, FlattenedCast, FlattenedProfile } from '../types/index'
  */
 export async function indexAllCasts() {
   const startTime = Date.now()
+
+  // Get all profiles
   const { data: _profiles, error: profilesError } = await supabase
     .from(profilesTable)
     .select('*')
@@ -23,13 +26,15 @@ export async function indexAllCasts() {
   console.log(`Indexing casts from ${_profiles.length} profiles...`)
   const profiles: FlattenedProfile[] = _profiles
 
-  const worker = await spawn(new Worker('./worker'))
   const chunks = breakIntoChunks(profiles, 500)
+  const worker = await spawn(new Worker('./worker'))
 
-  console.log(`There are ${chunks.length} chunks`)
+  console.log(`There are ${chunks.length} chunks to process`)
 
+  // Process each chunk independently
   await Promise.all(chunks.map(chunk => worker.saveCastsForChunk(chunk)))
 
+  // Terminate all threads
   await Thread.terminate(worker)
 
   const endTime = Date.now()
@@ -50,32 +55,7 @@ export async function indexAllCastsForUser(address: string) {
     // don't save recasts
     if (cast.meta?.recast) return
 
-    allCasts.push({
-      type: 'text-short',
-      published_at: new Date(cast.body.publishedAt),
-      sequence: cast.body.sequence,
-      address: cast.body.address,
-      username: cast.body.username,
-      text: cast.body.data.text,
-      reply_parent_merkle_root: cast.body.data.replyParentMerkleRoot || null,
-      prev_merkle_root: cast.body.prevMerkleRoot || null,
-      signature: cast.signature,
-      merkle_root: cast.merkleRoot,
-      thread_merkle_root: cast.threadMerkleRoot,
-      display_name: cast.meta?.displayName || null,
-      avatar_url: cast.meta?.avatar || null,
-      avatar_verified: cast.meta?.isVerifiedAvatar || false,
-      mentions: cast.meta?.mentions || [],
-      num_reply_children: cast.meta?.numReplyChildren || null,
-      reply_parent_username: cast.meta?.replyParentUsername?.username || null,
-      reply_parent_address: cast.meta?.replyParentUsername?.address || null,
-      reactions: cast.meta?.reactions?.count || null,
-      recasts: cast.meta?.recasts?.count || null,
-      watches: cast.meta?.watches?.count || null,
-      recasters: cast.meta?.recasters || [],
-      deleted: cast.body.data.text.startsWith('delete:farcaster://') ? true : false,
-      recast: cast.body.data.text.startsWith('recast:') ? true : false,
-    })
+    allCasts.push(flattenCast(cast))
   })
 
   // Break allCasts into chunks of 1000
@@ -114,23 +94,4 @@ export async function indexAllCastsForUser(address: string) {
   }
 
   console.log(`Saved ${allCasts.length} casts from ${address} profiles`)
-}
-
-/**
- * Iterate through all the pages of a profile's activity and return the full activity
- * @param profile Flattened Farcaster profile from Supabase
- * @returns Array of all casts by a user
- */
-async function getProfileActivity(profile: FlattenedProfile): Promise<Cast[]> {
-  const _activity = await got(
-    `https://guardian.farcaster.xyz/origin/address_activity/${profile.address}`,
-  )
-    .json()
-    .catch(err => {
-      console.error(`Could not get activity for @${profile.username}`, err)
-      return []
-    })
-
-  const activity = _activity as Cast[]
-  return activity
 }
--- src/functions/worker.ts
+++ src/functions/worker.ts
@@ -3,6 +3,7 @@ import got from 'got'
 import supabase from '../supabase.js'
 import { castsTable } from '../index.js'
 import { expose } from 'threads/worker'
+import flattenCast from '../utils/flattenCast.js'
 
 expose({
   async saveCastsForChunk(chunk: FlattenedProfile[]) {
@@ -14,32 +15,7 @@ expose({
       const userCasts: FlattenedCast[] = []
 
       activity.map((cast: Cast) => {
-        userCasts.push({
-          type: 'text-short',
-          published_at: new Date(cast.body.publishedAt),
-          sequence: cast.body.sequence,
-          address: cast.body.address,
-          username: cast.body.username,
-          text: cast.body.data.text,
-          reply_parent_merkle_root: cast.body.data.replyParentMerkleRoot || null,
-          prev_merkle_root: cast.body.prevMerkleRoot || null,
-          signature: cast.signature,
-          merkle_root: cast.merkleRoot,
-          thread_merkle_root: cast.threadMerkleRoot,
-          display_name: cast.meta?.displayName || null,
-          avatar_url: cast.meta?.avatar || null,
-          avatar_verified: cast.meta?.isVerifiedAvatar || false,
-          mentions: cast.meta?.mentions || [],
-          num_reply_children: cast.meta?.numReplyChildren || null,
-          reply_parent_username: cast.meta?.replyParentUsername?.username || null,
-          reply_parent_address: cast.meta?.replyParentUsername?.address || null,
-          reactions: cast.meta?.reactions?.count || null,
-          recasts: cast.meta?.recasts?.count || null,
-          watches: cast.meta?.watches?.count || null,
-          recasters: cast.meta?.recasters || [],
-          deleted: cast.body.data.text.startsWith('delete:farcaster://') ? true : false,
-          recast: cast.body.data.text.startsWith('recast:') ? true : false,
-        })
+        userCasts.push(flattenCast(cast))
       })
 
       const { error } = await supabase.from(castsTable).upsert(userCasts, {
