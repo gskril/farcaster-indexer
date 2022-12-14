ca3d2577074fce91d1c0117dbd33558587146869
Greg <gskril@gmail.com>
2022-12-13 19:18:38 -0500

    Keep "casts" table plural because of reserved word

--- supabase/migrations/20221213165543_singular-tables.sql
+++ supabase/migrations/20221213165543_singular-tables.sql
@@ -1,3 +1,2 @@
-ALTER TABLE IF EXISTS public.profiles RENAME TO "profile";
-ALTER TABLE IF EXISTS public.casts RENAME TO "cast";
-ALTER TABLE IF EXISTS public.verifications RENAME TO "verification";
+ALTER TABLE IF EXISTS public.profiles RENAME TO profile;
+ALTER TABLE IF EXISTS public.verifications RENAME TO verification;
