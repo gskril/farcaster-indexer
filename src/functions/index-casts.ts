e3ef6a7b2fcb565b9574c4bfe0224bf53a65acab
Greg <gskril@gmail.com>
2022-12-18 03:06:21 -0500

    Tweak logging

--- src/functions/index-casts.ts
+++ src/functions/index-casts.ts
@@ -54,8 +54,8 @@ export async function indexAllCasts(limit?: number) {
   const endTime = Date.now()
   const duration = (endTime - startTime) / 1000
 
-  if (duration > 45) {
-    // If it takes more than 45 seconds, log the duration so we can optimize
+  if (duration > 60) {
+    // If it takes more than 60 seconds, log the duration so we can optimize
     console.log(`Updated ${formattedCasts.length} casts in ${duration} seconds`)
   }
 }
--- src/functions/update-profiles.ts
+++ src/functions/update-profiles.ts
@@ -42,8 +42,8 @@ export async function updateAllProfiles() {
   const endTime = Date.now()
   const duration = (endTime - startTime) / 1000
 
-  if (duration > 45) {
-    // If it takes more than 45 seconds, log the duration so we can optimize
+  if (duration > 60) {
+    // If it takes more than 60 seconds, log the duration so we can optimize
     console.log(`Updated ${allProfiles.length} profiles in ${duration} seconds`)
   }
 }
