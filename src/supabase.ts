0bf2b26ccbe0debe6cd1652179827f46e9337f27
Greg <gskril@gmail.com>
2023-03-11 13:06:18 -0500

    Generate db types

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { Database } from './types/supabase'
+
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_KEY) {
   throw new Error('Missing Supabase environment variables')
 }
 
-const supabase = createClient(
+const supabase = createClient<Database>(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
 )
