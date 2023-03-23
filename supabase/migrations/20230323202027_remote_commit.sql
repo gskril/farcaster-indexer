32e5e797d210abea55ff0f03f05273a496c56876
Michael Wang <michaelwang11394@gmail.com>
2023-03-23 13:29:19 -0700

    Farcaster index init

---
+++ supabase/migrations/20230323202027_remote_commit.sql
@@ -0,0 +1,17 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION storage.can_insert_object(bucketid text, name text, owner uuid, metadata jsonb)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  INSERT INTO "storage"."objects" ("bucket_id", "name", "owner", "metadata") VALUES (bucketid, name, owner, metadata);
+  -- hack to rollback the successful insert
+  RAISE sqlstate 'PT200' using
+  message = 'ROLLBACK',
+  detail = 'rollback successful insert';
+END
+$function$
+;
+
+
