546d6a3f97aaeaf56fc21ccf09f5ceaadfe54ce7
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-12-23 22:42:39 -0500

    Add function to fetch casts by URL

--- supabase/migrations/20231223214521_embeds_and_tags.sql
+++ supabase/migrations/20231223214521_embeds_and_tags.sql
@@ -2,4 +2,14 @@ alter table "public"."casts" add column "embeds" jsonb;
 
 alter table "public"."casts" add column "tags" jsonb[];
 
+CREATE INDEX idx_embeds ON "public"."casts" USING GIN ("embeds");
 
+CREATE OR REPLACE FUNCTION find_casts_by_url(dynamic_url TEXT)
+RETURNS SETOF casts AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM casts
+    WHERE embeds @> jsonb_build_object('urls', jsonb_build_array(jsonb_build_object('openGraph', jsonb_build_object('url', dynamic_url))));
+END;
+$$ LANGUAGE plpgsql;
