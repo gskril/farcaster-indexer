b4026623946e42624f448db644aed219516008fc
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-02-19 00:29:28 -0500

    Use enums

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -1,4 +1,4 @@
-import { HubEvent, MessageType } from '@farcaster/hub-nodejs'
+import { HubEvent, HubEventType, MessageType } from '@farcaster/hub-nodejs'
 
 import { deleteCast, insertCasts } from '../api/cast.js'
 import { insertReactions } from '../api/reaction.js'
@@ -10,8 +10,8 @@ import { deleteVerification, insertVerifications } from '../api/verification.js'
  * @param event Hub event in JSON format
  */
 export async function handleEvent(event: HubEvent) {
-  // Handle each event type: MERGE_MESSAGE (1), PRUNE_MESSAGE (2), REVOKE_MESSAGE (3), MERGE_ID_REGISTRY_EVENT (4), MERGE_NAME_REGISTRY_EVENT (5)
-  if (event.type === 1) {
+  // Handle each event type: MERGE_MESSAGE, PRUNE_MESSAGE, REVOKE_MESSAGE (3), MERGE_ID_REGISTRY_EVENT (4), MERGE_NAME_REGISTRY_EVENT (5)
+  if (event.type === HubEventType.MERGE_MESSAGE) {
     event.mergeMessageBody?.message?.data?.type
     event.mergeMessageBody!
     const msg = event.mergeMessageBody!.message!
@@ -33,39 +33,14 @@ export async function handleEvent(event: HubEvent) {
       // TODO: figure out how to track reaction deletes. There is nothing like msg.data.reactionRemoveBody
       // await deleteReaction(msg)
     }
-    // } else if (event.type === 2) {
-    //   // Mark the relevant row as `pruned` in the db but don't delete it
-    //   const msg = event.message as MergeMessageHubEvent
-    //   const msgType = msg.data.type
-    //   if (msgType === 'MESSAGE_TYPE_CAST_ADD') {
-    //     await updateCast(msg.hash, { pruned: true })
-    //   } else if (msgType === 'MESSAGE_TYPE_VERIFICATION_ADD_ETH_ADDRESS') {
-    //     await updateVerification(msg, { pruned: true })
-    //   } else if (msgType === 'MESSAGE_TYPE_REACTION_ADD') {
-    //     await updateReaction(msg, { pruned: true })
-    //   } else if (msgType === 'MESSAGE_TYPE_SIGNER_ADD') {
-    //     await updateSigner(msg, { pruned: true })
-    //   }
-    // } else if (event.type === 3) {
-    //   // Events are emitted when a signer that was used to create a message is removed
-    //   // We take care of this within the `MESSAGE_TYPE_SIGNER_REMOVE` message for all entities except profiles
-    //   const msg = event.message as MergeMessageHubEvent
-    //   const msgType = msg.data.type
-    //   if (msgType === 'MESSAGE_TYPE_USER_DATA_ADD') {
-    //     await deletePartOfProfile(msg)
-    //   }
-    // } else if (event.type === 4) {
-    //   const msg = event.message as protobufs.IdRegistryEvent
-    //   // Handle all event: REGISTER (1), TRANSFER (2)
-    //   if (msg.type === 1) {
-    //     await insertProfile(msg)
-    //   } else if (msg.type === 2) {
-    //     await updateProfileOwner(msg)
-    //   }
-    // } else if (event.type === 5) {
-    //   const msg = event.message as protobufs.NameRegistryEvent
-    //   // TODO: decode msg.fname
-    //   console.log('MERGE_NAME_REGISTRY_EVENT', msg.fname)
+  } else if (event.type === HubEventType.PRUNE_MESSAGE) {
+    // TODO: Mark the relevant row as `pruned` in the db but don't delete it
+    // Not important right now because I don't want to prune data for my applications
+  } else if (event.type === HubEventType.REVOKE_MESSAGE) {
+    // Events are emitted when a signer that was used to create a message is removed
+    // TODO: handle revoking messages
+  } else if (event.type === HubEventType.MERGE_ON_CHAIN_EVENT) {
+    // TODO: handle onchain events
   } else {
     console.log('UNHANDLED_HUB_EVENT', event.id)
   }
