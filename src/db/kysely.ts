4c1cbb825fc9c922b7b2fb0cc2e43a0645daf255
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-03-04 17:05:08 -0500

    Support Neon postgres

--- src/db/kysely.ts
+++ src/db/kysely.ts
@@ -1,18 +1,29 @@
 import { CamelCasePlugin, Kysely, PostgresDialect } from 'kysely'
+import { NeonDialect } from 'kysely-neon'
 import Pool from 'pg-pool'
+import ws from 'ws'
 
 import { Tables } from './db.types'
 
-if (!process.env.DATABASE_URL) {
+const DATABASE_URL = process.env.DATABASE_URL
+if (!DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
 }
 
-export const db = new Kysely<Tables>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      max: 10,
-      connectionString: process.env.DATABASE_URL,
-    }),
+const basicPostgresDialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: DATABASE_URL,
   }),
+})
+
+const neonDialect = new NeonDialect({
+  connectionString: DATABASE_URL,
+  webSocketConstructor: ws,
+})
+
+export const db = new Kysely<Tables>({
+  dialect: DATABASE_URL.includes('neon.tech')
+    ? neonDialect
+    : basicPostgresDialect,
   plugins: [new CamelCasePlugin()],
 })
